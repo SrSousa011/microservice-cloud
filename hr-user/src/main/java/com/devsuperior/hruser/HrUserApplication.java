4eab7807da5472ba5835bdc718a6c57c2c368c23
Lucas Sousa <lucas.sousa_99@hotmail.com>
2022-10-03 10:18:43 -0300

    [ADD] Project Hr-User

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,11 +1,13 @@
 package com.devsuperior.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
